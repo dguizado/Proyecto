dc31705d3ff7bfa37e490ec87dae3fd2212bba5d
dguizado <angigiacolemar@hotmail.com>
2017-06-24 22:41:51 -0500

    Funciones Conteo y Buscar

--- funcionBuscar.c
+++ funcionBuscar.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
+#include<>
 #include"miLista.h"
 
-extern ElementoLista *Lista_Buscar(ListaEnlazada *lista, void *objeto){
-
+ElementoLista *Lista_Buscar(ListaEnlazada *lista, void *objeto){
+   ElementoLista *nuevo, *elemento;
+   nuevo = (ElementoLista*) malloc(sizeof(ElementoLista));
+   elemento = (ElementoLista*) malloc(sizeof(ElementoLista));
+   nuevo->objeto = objeto;
+   for(nuevo=lista->ancla.siguiente; nuevo=lista->ancla.anterior; nuevo++){
+      if(nuevo->objeto == elemento->objeto){
+        return nuevo;
+      }
+   }
 }
+
--- funcionConteo.c
+++ funcionConteo.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 #include "miLista.h"
 
-extern int Lista_Conteo(ListaEnlazada *lista){
+int Lista_Conteo(ListaEnlazada *lista){
+    ElementoLista *i;
+    int count=0;
+    for (i= lista->ancla.siguiente; i = lista->ancla.anterior; i++){
+        count ++;
+    }
+  return count;
 
 }
